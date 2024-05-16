b3e6b5bbdd889343d86ec813ff41f4ee8436494b
mogbonna <mogbonna045@gmail.com>
2024-05-16 19:05:56 +0000

    Doing Hard thing is Hard

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,11 +6,9 @@ import requests
 from functools import wraps
 from typing import Callable
 
-
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
-
 
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
@@ -24,15 +22,13 @@ def data_cacher(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
-
 
 @data_cacher
 def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
-    return requests.get(url).text
+    return requests.get(url).tex
